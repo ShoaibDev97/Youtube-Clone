2193e2d505ec60c44ad27b27d7485c00f85fd6ea
ShoaibDev97 <shoaibmuhammadarif@gmail.com>
2021-07-14 12:51:01 +0500

    Updates

---
+++ precache-manifest.726934b87da5bd9744c1d6834b8dabdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e90c4c4b8a17991bbf457398a4be39",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "22c841a9d4fc9595589f",
+    "url": "/youtube-clone/static/css/main.f4afbd2e.chunk.css"
+  },
+  {
+    "revision": "6acd4eb15dfe02c5fe3d",
+    "url": "/youtube-clone/static/js/2.4fe04cc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone/static/js/2.4fe04cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c841a9d4fc9595589f",
+    "url": "/youtube-clone/static/js/main.563de1e9.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
